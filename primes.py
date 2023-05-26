6998d4b19e5f91b1adafebe4f7f93d4d6aaa293c
Srinathpeddapalli <srinathchary30@gmail.com>
2023-05-26 22:37:11 +0530

    Improved one

--- primes.py
+++ primes.py
@@ -1,14 +1,13 @@
-def is_prime(num):
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
 def generate_primes(N):
     primes = []
     for num in range(2, N + 1):
-        if is_prime(num):
+        is_prime = True
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                is_prime = False
+                break
+        if is_prime:
             primes.append(num)
     return primes
+
+
